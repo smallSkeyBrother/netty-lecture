be02c6798a6a01ef30ad2185cf0e0fc872fd6025
xiaotiangege <361807535@qq.com>
2019-05-21 23:34:05 +0800

    frist commit

---
+++ src/main/java/com/xiaotiangege/netty/firstexample/TestServer.java
@@ -0,0 +1,25 @@
+package com.xiaotiangege.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();//事件循环组
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)//
+                    .childHandler(new TestServerInitializer());
+            ChannelFuture channelFuture = b.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
