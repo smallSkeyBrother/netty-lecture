4be920af837f1736fe6d0ec6ee6745b63adc4011
xiaotiangege <361807535@qq.com>
2019-05-22 23:28:19 +0800

    群聊实现

---
+++ src/main/java/com/xiaotiangege/netty/thirdexample/MyChatServer.java
@@ -0,0 +1,25 @@
+package com.xiaotiangege.netty.thirdexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyChatServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boss, worker).channel(NioServerSocketChannel.class).childHandler(new MyChatServerInitializer());
+
+            ChannelFuture future = b.bind(8899).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
